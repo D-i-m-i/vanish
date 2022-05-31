1edef1fb0ede71eab87a00997a96a2fc414822bd
Claire LIN <chunhuilin7516@gmail.com>
2022-05-31 12:40:01 +0800

    add validations to booking/user/van models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :van
+
+  validates :start_date, presence: true
+  validates :start_date, comparison: { greater_than: :end_date }
+  validates :end_date, presence: true
+  validates :total_price, numericality: true
 end
--- app/models/van.rb
+++ app/models/van.rb
@@ -1,4 +1,13 @@
 class Van < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :name, presence: true
+  validates :overview, presence: true
+  validates :overview, length: { maximum: 500 }
+  validates :make_model_year, presence: true
+  validates :kitchenette, inclusion: { in: [true, false] }
+  validates :toilet, inclusion: { in: [true, false] }
+  validates :solar_power, inclusion: { in: [true, false] }
+  validates :price, numericality: true
 end
