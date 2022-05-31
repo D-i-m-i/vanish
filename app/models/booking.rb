1a31a180ca14968ea16c05de687a2e9171b7ca57
Claire LIN <chunhuilin7516@gmail.com>
2022-05-31 14:27:48 +0800

    revise start_date end_date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :van
 
-  validates :start_date, presence: true
-  validates :start_date, comparison: { greater_than: :end_date }
-  validates :end_date, presence: true
   validates :total_price, numericality: true
+  validate :end_after_start
+  validates :start_date, :end_date, :presence => true
+
+  private
+  def end_after_start
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
