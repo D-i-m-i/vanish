6af3b34a8ac01e5056887bc92cdf39b79163aa4c
Maxime Carrat <maximecarrat@gmail.com>
2022-05-31 17:19:08 +0800

    added van index, show, and new controllers / views

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,10 +1,28 @@
 class VansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
   def index
 		@vans = Van.all
   end
 
   def show
     @van = Van.find(params[:id])
+  end
+
+  def new
+    @van = Van.new
+  end
+
+  def create
+    @van = Van.new(van_params)
+    if @van.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    flash[:alert] = "Van created successfully"
+    redirect_to van_path(@van)
+    else
+      flash[:alert] = "Error #{ @van.errors.objects.first.full_message}"
+      render :new
+    end
   end
 
   private
