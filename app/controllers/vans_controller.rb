d96124e97a1853561fa4d9ece1b78b9c4a23a364
Maxime Carrat <maximecarrat@gmail.com>
2022-05-31 15:50:56 +0800

    completed show controller and views

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -2,4 +2,14 @@ class VansController < ApplicationController
   def index
 		@vans = Van.all
   end
+
+  def show
+    @van = Van.find(params[:id])
+  end
+
+  private
+
+  def van_params
+    params.require(:van).permit(:name, :overview, :make_model_year, :kitchenette, :toilet, :solar_power, :price)
+  end
 end
