8697cb542eff571b40ff89891ed8976612a382b3
Claire Lin <chunhuilin7516@gmail.com>
2022-06-03 15:49:34 +0800

    finish user-dashboard

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,9 @@
 class ProfilesController < ApplicationController
   def dashboard
-    @your_trips = current_user.bookings.where("start_date > ?", Date.today)
+    @pending_trips = current_user.bookings.where("start_date > ?", Date.today).where(status: "pending")
+    @upcoming_trips = current_user.bookings.where("start_date > ?", Date.today).where(status: "accepted")
     @past_trips = current_user.bookings.where("end_date < ?", Date.today)
-    @pending_bookings = current_user.your_bookings
+    @pending_bookings = current_user.your_bookings.where(status: "pending")
     @past_bookings = current_user.your_bookings.where("end_date < ?", Date.today)
   end
 end
