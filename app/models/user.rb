bb871f940c621741f3e234b7a0e5dbe24eeff7ee
Claire Lin <chunhuilin7516@gmail.com>
2022-06-03 15:12:00 +0800

    work on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :vans
+  has_many :your_bookings, through: :vans, source: :bookings
   has_one_attached :photo
 
   validates :name, presence: true
